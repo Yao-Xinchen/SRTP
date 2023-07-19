c42c4cd0ef68003eb2a2aff24f00e3d35ae9307d
Yao-Xinchen <yao29@illinois.edu>
2023-07-19 16:19:24 +0800

    change root directory to src

--- build/custom_interface/rosidl_generator_py/custom_interface/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from custom_interface.msg._position import Position  # noqa: F401
-from custom_interface.msg._torque import Torque  # noqa: F401
--- build/dynamixel_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xinchen/Dev/SRTP/src/DynamixelSDK/dynamixel_sdk
-# Build directory: /home/xinchen/Dev/SRTP/build/dynamixel_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
