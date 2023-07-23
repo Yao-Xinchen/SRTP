4e835acf508877850df5924d129929f90b6f822d
Yao-Xinchen <yao29@illinois.edu>
2023-07-23 21:19:43 +0800

    hand, glove scheduler, and their interface added

---
+++ hand_scheduler/hand_scheduler/hand_scheduler.py
@@ -0,0 +1,23 @@
+# the robot hand measures angles resulted from the environment
+# and sends them to the glove
+
+# it receives the current torque of the human hand
+# and sends it to the environment
+
+goal_angle = [[0.0,0.0,0.0]
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0]
+    ]
+# unit: radian
+
+present_torque = [[0.0,0.0,0.0]
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0]
+    ]
+# unit: Nm
